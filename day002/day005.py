e3b6457f4cd6080351f3e09bc88e2d95ccde5772
junw <45444154+wo1261931780@users.noreply.github.com>
2022-11-04 13:59:47 +0800

    day2-12

--- day002/day005.py
+++ day002/day005.py
@@ -4,11 +4,14 @@ weight = 100
 
 print("名字%s，年龄%s，体重%s" % (name, age, weight))
 # 可以全部作为字符串输出
+print("名字%s，年龄%s，体重%s" % (name, age, weight + 1))  # 除了直接引入变量以外，还可以手动对变量做加减法
+
 print(f"我的名字{name}，今年{age}岁")
 # 效果一样，但是更加高效
 
 # 这里注意一下转义符
 # /这是斜杠，\这是反斜杠
+# 右下反斜杠，斜杠在shift旁边，反斜杠在enter上面
 print("demo1")
 print("demo2")
 print("demo1\ndemo2")  # 转义字符只会转化一个单词，中间如果添加空格，会不符合我们需要的排版
---
+++ day002/day008.py
@@ -0,0 +1,3 @@
+print("下面是输入测试：")
+str1 = input("输入内容：")
+print(f"我是str1的结果：{str1}")
