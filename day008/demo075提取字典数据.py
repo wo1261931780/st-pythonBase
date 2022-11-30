a3945a0f5645d77b2082a57cce040c701b1f1d1f
junw <45444154+wo1261931780@users.noreply.github.com>
2022-11-30 22:44:45 +0800

    day9-1

--- day008/demo075提取字典数据.py
+++ day008/demo075提取字典数据.py
@@ -1,4 +1,3 @@
-print("==================================================")
 dict1 = {"demo1": 100, "demo2": 166, "demo3": 150, "demo4": 200}
 dict2 = {i: j for i, j in dict1.items() if j > 151}
 print(dict2)  # {'demo2': 166, 'demo4': 200}
