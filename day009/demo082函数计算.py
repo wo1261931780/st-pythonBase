c72ba0b9c3694057ab99e51117d9bd9fbaf282bf
junw <45444154+wo1261931780@users.noreply.github.com>
2022-12-02 12:56:35 +0800

    day9-finish

---
+++ day009/demo082函数计算.py
@@ -0,0 +1,14 @@
+print("==================================================")
+
+
+def add_nums(num1, num2, num3):
+    return num1 + num2 + num3
+
+
+def revenge_nums():
+    return add_nums(2, 3, 4) / 3
+
+
+# python中，只要涉及到除法，得到的都是浮点数
+
+print(revenge_nums())
