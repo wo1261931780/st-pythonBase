df8099978cb5447e8abccf35848f2546ccfb10a3
junw <45444154+wo1261931780@users.noreply.github.com>
2022-12-11 17:06:11 +0800

    day13-13

--- day13/demo115seek函数.py
+++ day13/demo115seek函数.py
@@ -1,4 +1,12 @@
-
-
-
+text_io = open("demo109.txt", "r+")
+# text_io_read = text_io.read()  # 默认读取所有数据
+# 换句话来说，指针在开头
+# print(text_io_read)
+# print("==================================================")
+# text_io.seek(2, 0)  # 跳过两个字符，修改指针位置
+# 第一个变量，表示偏移量
+# 第二个变量，表示指针的起始位置
+text_io.seek(0, 2)
+print(text_io.read())
 print("==================================================")
+text_io.close()
---
+++ day13/demo116文件备份.py
@@ -0,0 +1,5 @@
+print("==================================================")
+input_str1 = input("请输入要备份的文件名：")  # 11.22.33
+print(input_str1)
+input_str__rfind = input_str1.rfind(".")
+print(input_str__rfind)  # 5,表示有多少个字符
