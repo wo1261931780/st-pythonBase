3c6c4af29218fa2352e816d0546d36b24cc20115
junw <45444154+wo1261931780@users.noreply.github.com>
2022-12-17 17:11:58 +0800

    day18-finish

---
+++ day018/demo158包.py
@@ -0,0 +1,12 @@
+print(1)
+
+
+def show_me():
+    print("show me the money")
+
+
+print("==================================================")
+
+import demo_p  # 导入demo_p这个包
+
+demo_p.show_me1()  # 导入包以后，直接访问内部的show1函数即可
